ec07ca1bbe9893ced12d2a63dbebfbfe4ab4c05c
Tiago Royer <royertiago@gmail.com>
2015-05-06 00:24:53 -0300

    catarinense/chaveiro.cpp: Provado que o problema é NP-completo.

--- problemas/catarinense/chaveiro.cpp
+++ problemas/catarinense/chaveiro.cpp
@@ -1,3 +1,86 @@
+/* Problema C da Maratona Catarinense de Programação de 2013.
+ *
+ * Teorema: este problema, com cadeias de caracteres de tamanho arbitrário,
+ * é NP-completo.
+ *
+ * Demonstração: Reduziremos CNF-SAT para este problema.
+ * Para uma fórmula lógica com N variáveis e M cláusulas,
+ * usaremos uma fechadura de tamanho 2M + 3
+ * e N + 1 chaves de tamanho 2M + 3.
+ * Apenas precisaremos de pinos de alturas 0, 1, 2 e 3.
+ *
+ * As N primeiras chaves codificarão variáveis.
+ * A ideia é restringir as posições destas chaves a apenas duas posições.
+ * Interpretaremos a primeira posição como atribuir V à variável correspondente,
+ * e a segunda posição como atribuir F à variável correspondente.
+ *
+ * Elas terão três pinos de tamanho 2, nas posições 0, M+1 e 2M+2.
+ * Todos os demais pinos possuirão tamanho 1 ou 0.
+ * A fechadura também terá três pinos de tamanho 2, nestas mesmas posições
+ * (com exceção do pino do meio, que terá altura 3; justificaremos mais tarde).
+ * Todos os outros pinos da fechadura terão altura 1.
+ *
+ * Observe que o pino da frente das chaves possui altura 2,
+ * que, por ser mais alto que a maior parte dos pinos,
+ * possui apenas três posições possíves em que pode ser encaixado:
+ * na posição 0, M+1 ou 2M+2. Todos estes encaixes são válidos.
+ * Embora não tenhamos conseguido restringir o posicionamento
+ * para apenas duas posições, em uma das posições a chave não interfere
+ * na combinação gerada, portanto, isto é bom o bastante.
+ *
+ * Interpretaremos "Inserir até a metade" como atribuir falso à variável
+ * e "Inserir até o fundo" como atribuir verdadeiro à variável.
+ *
+ * Agora, às cláusulas: Se a variável K aparece na cláusula J
+ * como um literal positivo, escolha o pino J+1 na chave K como 1;
+ * caso contrário, escolha como 0.
+ * Se a variável K aparece na cláusula J como um literal negativo,
+ * escolha o pino M+1 + J+1 como 1; caso contrário, escolha como 0.
+ *
+ * Concentremo-nos nos M primeiros pinos da fechadura (após o pino inical,
+ * de tamanho 2). Caso escolhamos a variável K como falsa
+ * (isto é, inserimos-na até a metade), os M+2 últimos pinos da chave
+ * serão inseridos como os M+2 primeiros pinos da fechadura.
+ * Portanto, se o pino M+1 + J+1 for 1, nós já garantimos que
+ * o pino J+1 da fechadura fique em 1. Observe que o pino M+1 + J+1
+ * é 1 exatamente quando a cláusula J possuir K como literal negativo;
+ * isto é, o pino J+1 na fechadura representa o valor-verdade
+ * da cláusula J na fórmula CNF da entrada.
+ * Similarmente, se K for verdadeiro, então todos os pinos
+ * que correspondem a cláusulas com K como literal positivo
+ * ficarão "acesos" na fechadura.
+ *
+ * Observe que um pino só fica "aceso" se alguma das chaves o "acender".
+ * Portanto, qualquer posicionamento das chaves
+ * que "acenda" todos os M+1 primeiros pinos da fechadura
+ * codifica uma atribuição que satisfaz a fórumula da entrada,
+ * e vice-versa.
+ * (Podemos entender a não-inserção da chave como uma variável não-atribuida.
+ * Se com uma atribuição parcial já conseguimos satisfazer à fórmula,
+ * escolher valores arbitrários para as demais variáveis
+ * resulta numa atribuição que satisfaz à formula,
+ * portanto este detalhe não nos traz problemas.)
+ *
+ * Falta resolver os M+1 últimos pinos.
+ * Para isso, adicionaremos mais uma chave, específica para esta situação.
+ * Ela também conterá os três pinos de "posicionamento",
+ * mas o pino central terá tamanho 3. É a única chave com um pino deste tamanho;
+ * como a fechadura também possui um pino de tamanho 3,
+ * esta chave é de "uso obrigatório" na solução.
+ * Escolhemos os pinos M+2 até 2M+1 desta chave como 1,
+ * apenas para garantir que os últimos pinos da fechadura sejam sempre preenchidos;
+ * e escolheremos os demais pinos como 0, para não influenciar
+ * na organização descrita no parágrafo anterior.
+ *
+ * Assim, qualquer atribuição satisfazível codifica um posicionamento das chave
+ * que resolve os M+1 primeiros pinos, e, usando esta chave adicional,
+ * nós resolvemos "no braço" os M+2 últimos;
+ * e, dado um posicionamento de chaves, conseguimos reconstruir
+ * a atribuição correspondente que satisfaz a fórmula.
+ *
+ * Portanto, todo problema em NP é redutível a este problema.
+ * Q.E.D.
+ */
 #include <algorithm>
 #include <iostream>
 #include <string>
