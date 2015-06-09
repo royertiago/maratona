fc1df89dafb0d8f465f3536f5960a61c00f65e37
Tiago Royer <royertiago@gmail.com>
2015-06-09 12:54:54 -0300

    Explicado spoj-br/fatorial.cpp.

--- problemas/spoj-br/fatorial.cpp
+++ problemas/spoj-br/fatorial.cpp
@@ -1,3 +1,25 @@
+/* Problema: http://br.spoj.com/problems/FATORIAL/
+ *
+ * A ideia é manter, num buffer, os dígitos menos significativos
+ * não nulos de n!.
+ * Por exemplo, para n = 5, armazenaremos 12 (pois n! = 120).
+ *
+ * Para obter os últimos dígitos de (n+1)! a partir dos últimos digitos de n!,
+ * bastaria multiplicá-los por (n+1);
+ * entretanto, eventualmente,
+ * a quantidade de dígitos não-nulos
+ * passará a ser maior do que o representável num inteiro.
+ *
+ * Portanto, precisamos apenas os k últimos dígitos.
+ * (Usaremos dois truques para minimizar a necessidade de armazenamento:
+ * primeiro, removeremos todos os múltiplos de 10 antes de múltiplicar por n+1;
+ * segundo, usaremos o fato de que multiplicar por 5 é mesmo que multiplicar por 10
+ * e depois dividir por 2 para tirar todos os fatores 5 do número n+1.)
+ *
+ * Quão grande k precisa ser?
+ * A maior potência de 5 menor que 10^6 é 5^8, portanto,
+ * manter os últimos 8 dígitos é suficiente.
+ */
 #include <stdio.h>
 
 int cache[(int) 1e6+1];
@@ -18,7 +40,7 @@ int solve( int input ) {
             j /= 5;
         }
 
-        cache[i] = d = (d * j) % (long long) 1e9;
+        cache[i] = d = (d * j) % (long long) 1e8;
     }
     largest_cached = input;
     return d % 10;
